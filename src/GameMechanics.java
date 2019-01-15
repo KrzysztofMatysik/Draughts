bd79bca1156d9b1f24aaee9e2b5c659ad316901e
KrzysztofMatysik <matysikrzysztof@gmail.com>
2019-01-15 11:19:43 +0100

    Change name class from "StartGame" to "GameMechanics".

--- src/GameMechanics.java
+++ src/GameMechanics.java
@@ -1,2 +1,31 @@
 public class GameMechanics {
+
+                                            /* Rules !!! */
+    // b - Black Pawns  || w - White Pawns || n - Black Queen    || e - White Queen || Twelve (12) Pawns for both sides
+    //
+
+    private String[] blackPawns ;
+    private String[] whitePawns;
+
+
+
+
+    public void DrawingBoard(){
+
+
+
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args){
+
+
+
+
+    }
 }
